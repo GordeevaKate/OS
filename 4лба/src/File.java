26b49c0bcd46601d2d9e8bd735a6061827f01320
GordeevaKate <55905714+GordeevaKate@users.noreply.github.com>
2020-05-12 16:07:20 +0400

    Update File.java

--- 4лба/src/File.java
+++ 4лба/src/File.java
@@ -1,12 +1,14 @@
 public class File {
 	private int fileSize;
 	private int fileId;
-
+	private Knot knots;
 	public File(int fileId, int fileSize) {
 		this.fileId = fileId;
 		this.fileSize = fileSize;
 	}
-
+        public Knot getKnot() {
+		return knots.nextNode();
+	}
 	public int getId() {
 		return fileId;
 	}
