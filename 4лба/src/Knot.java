9f6089de356aa7e84a0d6211c70a121b84c73c56
GordeevaKate <55905714+GordeevaKate@users.noreply.github.com>
2020-05-12 16:13:42 +0400

    Update Knot.java

--- 4лба/src/Knot.java
+++ 4лба/src/Knot.java
@@ -1,38 +1,26 @@
 public class Knot {
 	private int knotId;
-	private int fileId;
 	private boolean selectFlag;
 	private Knot knot;
+
 	public Knot(int fileId, int knotId) {
-		this.fileId = fileId;
 		this.knotId = knotId;
 	}
-
-	public void setFile(int fileId) {
-		this.fileId = fileId;
-	}
-
 	public void setKnot(int fileId, int knotId) {
 		knot = new Knot(fileId, knotId);
 	}
-
 	public Knot nextNode() {
 		return knot;
 	}
-
 	public int getKnotId() {
 		return knotId;
 	}
-
 	public boolean getSelectFlag() {
 		return selectFlag;
 	}
-
 	public void setSelectFlag(boolean selectFlag) {
 		this.selectFlag = selectFlag;
 	}
 
-	public int getId() {
-		return fileId;
-	}
 }
+
